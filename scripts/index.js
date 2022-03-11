24c7bf41222e55c80db7d581541f268940a81c52
Gleb Ryzhkov <apokeks2993@yandex.ru>
2022-03-11 17:34:12 +0500

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 //объявляем переменные для открыия и закрытия попапа
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupOpenEditButtonElement = document.querySelector('.profile__edit-button');
 
 //объявляем переменные для изменения и сохранения "имя" и "о себе"
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input-name');
-const descriptionInput = formElement.querySelector('.popup__input-description');
+const nameInput = formElement.querySelector('.popup__input_name');
+const descriptionInput = formElement.querySelector('.popup__input_description');
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileDescription = profile.querySelector('.profile__description');
+
+
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
@@ -29,5 +31,10 @@ const closePopup = function() {
   popupElement.classList.remove('popup_opened');
 };
 
-popupOpenButtonElement.addEventListener('click', openPopup);
+popupOpenEditButtonElement.addEventListener('click', function() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+  openPopup();
+});
+
 popupCloseButtonElement.addEventListener('click', closePopup);
